e7ae84c85ef88208da5a90ec414997c8b46a3c38
Simon JAILLET <sjaillet@gmail.com>
2015-05-28 01:01:59 +0200

    Refactors specs related to attributes.

--- spec/node/patcher/attrs-spec.js
+++ spec/node/patcher/attrs-spec.js
@@ -7,24 +7,10 @@ describe("attrs", function() {
 
   describe(".patch()", function() {
 
-    it('can sets the `"name"` attributes', function() {
+    it("sets an attributes", function() {
 
-      var node = h({ tagName: "input", attrs: { name: "input1" } });
-      var element = node.render();
-      expect(element.getAttribute("name")).toBe("input1");
-
-    });
-
-    it("sets attributes", function() {
-
-      var from = h({ tagName: "div" });
-
-      var to = h({
-        tagName: "div",
-        attrs: {
-          src: "test.jpg"
-        }
-      });
+      var from = h();
+      var to = h({ attrs: { src: "test.jpg" } });
 
       var rootNode = from.render();
       var newRoot = patch.node(from, to);
@@ -34,59 +20,58 @@ describe("attrs", function() {
 
     });
 
-    it("unsets attributes", function() {
+    it('sets the `"name"` attributes', function() {
 
-      var from = h({
-        tagName: "div",
-        attrs: {
-          a: "1",
-          b: "2",
-          c: "3"
-        }
-      });
-
-      var to = h({
-        tagName: "div",
-        attrs: {
-          a: "1",
-          c: "3"
-        }
-      });
-
-      var rootNode = from.render();
-      var newRoot = patch.node(from, to);
-
-      expect(newRoot).toBe(rootNode);
-      expect(newRoot.getAttribute("a")).toBe("1");
-      expect(newRoot.getAttribute("b")).toBe(null);
-      expect(newRoot.getAttribute("c")).toBe("3");
+      var node = h({ tagName: "input", attrs: { name: "input1" } });
+      var element = node.render();
+      expect(element.getAttribute("name")).toBe("input1");
 
     });
 
-    it("unsets all attributes", function() {
+    it('sets the `"style"` attributes', function() {
 
-      var from = h({
-        tagName: "div",
-        attrs: {
-          a: "1",
-          b: "2",
-          c: "3"
-        }
-      });
+      var from = h();
+      var to = h({ tagName: "div", attrs: { style: { display: "none" } } });
 
-      var to = h({ tagName: "div" });
+      from.render();
+      var elem = patch.node(from, to);
 
-      var rootNode = from.render();
-      var newRoot = patch.node(from, to);
-
-      expect(newRoot).toBe(rootNode);
-      expect(newRoot.getAttribute("a")).toBe(null);
-      expect(newRoot.getAttribute("b")).toBe(null);
-      expect(newRoot.getAttribute("c")).toBe(null);
+      expect(elem.style.display).toBe(_.style("display", "none"));
 
     });
 
-    it("patches style correctly", function() {
+    it("ignores `undefined` attribute", function() {
+
+      var node = h({ tagName: "div", attrs: { "class": undefined } });
+      var rootNode = node.render();
+      expect(rootNode.hasAttribute("class")).toBe(false);
+
+    });
+
+    it("ignores `undefined` value attribute", function() {
+
+      var node = h({ tagName: "div", attrs: { value: undefined } });
+      var rootNode = node.render();
+      expect("value" in rootNode).toBe(false);
+      expect(rootNode.hasAttribute("value")).toBe(false);
+
+    });
+
+    it("patches an attribute", function() {
+
+      var from = h({ attrs: { foo: "bar", bar: "oops" } });
+      var to = h({ attrs: { foo: "baz", bar: "oops" } });
+      var rootNode = from.render();
+      expect(rootNode.getAttribute("foo")).toBe("bar");
+      expect(rootNode.getAttribute("bar")).toBe("oops");
+
+      var newRoot = patch.node(from, to);
+      expect(newRoot.getAttribute("foo")).toBe("baz");
+      expect(newRoot.getAttribute("bar")).toBe("oops");
+
+    });
+
+    it('patches the `"style"` attribute', function() {
 
       var from = h({ tagName: "div", attrs: { style: { width: "10px" } } });
       var to = h({ tagName: "div", attrs: { style: { padding: "5px" } } });
@@ -171,125 +156,97 @@ describe("attrs", function() {
 
     });
 
-    describe("unsets properties", function() {
+    it('casts empty `"value"` attribute as empty string', function() {
 
-      it("removes all property if set to undefined", function() {
+      var from = h({ tagName: "input", attrs: { value: "hello" } });
+      var to = h({ tagName: "input", attrs: { value: null } });
 
-        var from = h({ tagName: "div", attrs: {
-          style: { display: "none", width: "10px" }
-        } });
+      from.render();
+      var rootNode = patch.node(from, to);
 
-        var to = h({ tagName: "div", attrs: { style: undefined } });
-
-        from.render();
-        var rootNode = patch.node(from, to);
-
-        expect(rootNode.style.display).toBe(_.style("display", ""));
-        expect(rootNode.style.width).toBe(_.style("width", ""));
-
-      });
-
-      it("removes a style if set to undefined", function() {
-
-        var from = h({ tagName: "div", attrs: { style: { display: "none" } } });
-        var to = h({ tagName: "div", attrs: { style: undefined } });
-
-        from.render();
-        var rootNode = patch.node(from, to);
-
-        expect(rootNode.style.display).toBe(_.style("display", ""));
-
-      });
-
-      it("allows `null` for removing an attribute", function() {
-
-        var from = h({ tagName: "input", attrs: { custom: "hello" } });
-        var to = h({ tagName: "input", attrs: { custom: null } });
-
-        from.render();
-        var rootNode = patch.node(from, to);
-
-        expect(rootNode.hasAttribute("custom")).toBe(false);
-        expect(rootNode.custom).toBe(undefined);
-
-      });
-
-      it("casts empty value as empty string", function() {
-
-        var from = h({ tagName: "input", attrs: { value: "hello" } });
-        var to = h({ tagName: "input", attrs: { value: null } });
-
-        from.render();
-        var rootNode = patch.node(from, to);
-
-        expect(rootNode.hasAttribute("value")).toBe(true);
-        expect(rootNode.value).toBe("");
-
-      });
+      expect(rootNode.hasAttribute("value")).toBe(true);
+      expect(rootNode.value).toBe("");
 
     });
 
-    describe("object as value", function() {
+    it("unsets an attribute", function() {
 
-      it("support style as object", function() {
+      var from = h({ attrs: { a: "1", b: "2", c: "3" } });
+      var to = h({ attrs: { a: "1", c: "3" } });
 
-        var from = h({ tagName: "div", attrs: {
-          style: {
-            border: "none",
-            className: "oops",
-            display: "none"
-          }
-        }});
+      var rootNode = from.render();
+      var newRoot = patch.node(from, to);
 
-        var to = h({ tagName: "div", attrs: {
-          style: {
-            border: "1px solid #000",
-            className: "oops",
-            display: ""
-          }
-        }});
+      expect(newRoot).toBe(rootNode);
+      expect(newRoot.getAttribute("a")).toBe("1");
+      expect(newRoot.getAttribute("b")).toBe(null);
+      expect(newRoot.getAttribute("c")).toBe("3");
 
-        var rootNode = from.render();
-        expect(rootNode.style.border).toBe(_.style("border", "none"));
-        expect(rootNode.style.className).toBe(_.style("className", "oops"));
-        expect(rootNode.style.display).toBe(_.style("display", "none"));
+    });
 
-        var newNode = to.render();
-        expect(newNode.style.border).toBe(_.style("border", "1px solid #000"));
-        expect(newNode.style.className).toBe(_.style("className", "oops"));
-        expect(newNode.style.display).toBe(_.style("display", ""));
+    it("unsets all attributes", function() {
 
-        var newRoot = patch.node(from, to);
-        expect(newRoot.style.border).toBe(_.style("border", "1px solid #000"));
-        expect(newRoot.style.className).toBe(_.style("className", "oops"));
-        expect(newRoot.style.display).toBe(_.style("display", ""));
-        expect(rootNode.style).toBe(newRoot.style);
+      var from = h({ attrs: { a: "1", b: "2", c: "3" } });
+      var to = h();
 
-      });
+      var rootNode = from.render();
+      var newRoot = patch.node(from, to);
 
-      it("support attributes as object", function() {
+      expect(newRoot).toBe(rootNode);
+      expect(newRoot.getAttribute("a")).toBe(null);
+      expect(newRoot.getAttribute("b")).toBe(null);
+      expect(newRoot.getAttribute("c")).toBe(null);
 
-        var from = h({ tagName: "div", attrs: { foo: "bar", bar: "oops" } });
-        var to = h({ tagName: "div", attrs: { foo: "baz", bar: "oops" } });
-        var rootNode = from.render();
+    });
 
-        var newRoot = patch.node(from, to);
-        expect(newRoot.getAttribute("foo")).toBe("baz");
-        expect(newRoot.getAttribute("bar")).toBe("oops");
+    it("unsets an attribute when equal to `undefined`", function() {
 
-      });
+      var from = h({ attrs: { style: { display: "none", width: "10px" } } });
 
-      it("patch nested properties in right only", function() {
+      var to = h({ attrs: { style: undefined } });
 
-        var from = h();
-        var to = h({ tagName: "div", attrs: { style: { display: "none" } } });
+      from.render();
+      var rootNode = patch.node(from, to);
 
-        from.render();
-        var elem = patch.node(from, to);
+      expect(rootNode.style.display).toBe(_.style("display", ""));
+      expect(rootNode.style.width).toBe(_.style("width", ""));
 
-        expect(elem.style.display).toBe(_.style("display", "none"));
+    });
 
-      });
+    it("unsets an attribute when equal to `null`", function() {
+
+      var from = h({ tagName: "input", attrs: { custom: "hello" } });
+      var to = h({ tagName: "input", attrs: { custom: null } });
+
+      from.render();
+      var rootNode = patch.node(from, to);
+
+      expect(rootNode.hasAttribute("custom")).toBe(false);
+      expect(rootNode.custom).toBe(undefined);
+
+    });
+
+    it('sets the `"style"` attribute using an object', function() {
+
+      var from = h({ attrs: { style: { border: "none", className: "oops", display: "none" } } });
+
+      var to = h({ attrs: { style: { border: "1px solid #000", className: "oops", display: "" } } });
+
+      var rootNode = from.render();
+      expect(rootNode.style.border).toBe(_.style("border", "none"));
+      expect(rootNode.style.className).toBe(_.style("className", "oops"));
+      expect(rootNode.style.display).toBe(_.style("display", "none"));
+
+      var newNode = to.render();
+      expect(newNode.style.border).toBe(_.style("border", "1px solid #000"));
+      expect(newNode.style.className).toBe(_.style("className", "oops"));
+      expect(newNode.style.display).toBe(_.style("display", ""));
+
+      var newRoot = patch.node(from, to);
+      expect(newRoot.style.border).toBe(_.style("border", "1px solid #000"));
+      expect(newRoot.style.className).toBe(_.style("className", "oops"));
+      expect(newRoot.style.display).toBe(_.style("display", ""));
+      expect(rootNode.style).toBe(newRoot.style);
 
     });
 
