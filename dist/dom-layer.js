85b99ca4e55e92b003f040e6629a591061eb878e
Simon JAILLET <sjaillet@gmail.com>
2019-01-17 13:00:57 +0100

    Create the 0.7.10 release.

--- dist/dom-layer.js
+++ dist/dom-layer.js
@@ -1,4 +1,4 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.domLayer = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.domLayer = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 var Tree = require('./src/tree/tree');
 var attach = require('./src/tree/attach');
 var render = require('./src/tree/render');
@@ -1419,7 +1419,6 @@ Tag.prototype.match = function(to) {
  */
 Tag.prototype.patch = function(to) {
   if (!this.match(to)) {
-    this.remove(false);
     return to.render(this.element.parentNode, to.parent);
   }
   to.element = this.element;
@@ -1620,7 +1619,6 @@ Text.prototype.match = function(to) {
  */
 Text.prototype.patch = function(to) {
   if (!this.match(to)) {
-    this.remove(false);
     return to.render(this.element.parentNode, to.parent);
   }
   to.element = this.element;
@@ -1773,9 +1771,13 @@ function patch(container, children, toChildren, parent) {
         toStartNode = toChildren[++toStartIndex];
       } else {
         node = fromChildren[index];
-        node.patch(toStartNode);
-        fromChildren[index] = undefined;
-        container.insertBefore(node.element, fromStartNode.element);
+        if (!node.match(toStartNode)) {
+          container.insertBefore(toStartNode.render(node.element.parentNode, toStartNode.parent), fromStartNode.element);
+        } else {
+          node.patch(toStartNode);
+          fromChildren[index] = undefined;
+          container.insertBefore(node.element, fromStartNode.element);
+        }
         toStartNode = toChildren[++toStartIndex];
       }
     }
@@ -1806,9 +1808,11 @@ function patch(container, children, toChildren, parent) {
 function keysIndexes(children, startIndex, endIndex) {
   var i, keys = Object.create(null), key;
   for (i = startIndex; i <= endIndex; ++i) {
-    key = children[i].key;
-    if (key !== undefined) {
-      keys[key] = i;
+    if (children[i]) {
+      key = children[i].key;
+      if (key !== undefined) {
+        keys[key] = i;
+      }
     }
   }
   return keys;
